77f3bf69fb99d4a6491818f76987bd8ef5ce5f3c
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-09 14:15:50 -0700

    fix: SideBar

--- app/components/side-bar/constants.ts
+++ app/components/side-bar/constants.ts
@@ -1,1 +1,6 @@
 export const SIDE_BAR_WIDTH = 200;
+
+export const SIDE_BAR_LINKS = [
+  { to: "/", children: "Home" },
+  { to: "/contact", children: "Contact" },
+];
--- app/components/side-bar/index.tsx
+++ app/components/side-bar/index.tsx
@@ -1,10 +1,13 @@
-import { Box, Button } from "@chakra-ui/react";
-import { SIDE_BAR_WIDTH } from "./constants";
+import { Button, Flex } from "@chakra-ui/react";
+import { SIDE_BAR_LINKS, SIDE_BAR_WIDTH } from "./constants";
+import { Link } from "@remix-run/react";
 
 export const SideBar = () => {
   return (
-    <Box
+    <Flex
       w={`${SIDE_BAR_WIDTH}px`}
+      direction="column"
+      gap="8px"
       bgColor="white"
       position="fixed"
       h="100%"
@@ -12,7 +15,9 @@ export const SideBar = () => {
       py="20px"
       right={0}
     >
-      <Button>Home</Button>
-    </Box>
+      {SIDE_BAR_LINKS.map((link) => {
+        return <Button as={Link} {...link} w="100%" key={link.to} />;
+      })}
+    </Flex>
   );
 };
