e4c31ba35233b9df6ae37d782d10a619d2450725
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-09 14:06:03 -0700

    chore: change code position

--- app/root.tsx
+++ app/root.tsx
@@ -17,17 +17,19 @@ import { Header } from "./components/header/index";
 import { SIDE_BAR_WIDTH } from "./components/side-bar/constants";
 import { SideBar } from "./components/side-bar";
 
-export const links: LinksFunction = () => {
-  return [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    { rel: "preconnect", href: "https://fonts.gstatic.com" },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
-    },
-    { rel: "icon", href: "/favicon.ico" },
-  ];
-};
+export default function App() {
+  return (
+    <Document>
+      <ChakraProvider theme={theme}>
+        <Header />
+        <SideBar />
+        <Box w={`calc(100dvw - ${SIDE_BAR_WIDTH}px)`}>
+          <Outlet />
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const locale = await i18next.getLocale(request);
@@ -65,16 +67,14 @@ const Document = ({ children }: DocumentProps) => {
   );
 };
 
-export default function App() {
-  return (
-    <Document>
-      <ChakraProvider theme={theme}>
-        <Header />
-        <SideBar />
-        <Box w={`calc(100dvw - ${SIDE_BAR_WIDTH}px)`}>
-          <Outlet />
-        </Box>
-      </ChakraProvider>
-    </Document>
-  );
-}
+export const links: LinksFunction = () => {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+    },
+    { rel: "icon", href: "/favicon.ico" },
+  ];
+};
