d64c2a53cf9ec1ebf164360de0468894a1d7acbb
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-12 18:56:39 -0700

    pref: load lib when it is necessary

--- app/routes/contact/route.tsx
+++ app/routes/contact/route.tsx
@@ -5,7 +5,6 @@ import {
   type MetaFunction,
   redirect,
 } from "@remix-run/node";
-import { Resend } from "resend";
 import i18next from "~/i18n/i18next.server";
 import { createMetaTitle } from "~/utils/createMetaTitle";
 import { Contact } from "./components/index";
@@ -18,6 +17,7 @@ export async function action({ request }: ActionFunctionArgs) {
   const body = await request.formData();
   try {
     // Ref: https://resend.com/docs/send-with-remix
+    const Resend = (await import("resend")).Resend;
     await new Resend(import.meta.env.RESEND_API_KEY).emails.send({
       from: `${String(body.get("email"))} <onboarding@resend.dev>`,
       to: ["tech.kupumaru@gmail.com"],
