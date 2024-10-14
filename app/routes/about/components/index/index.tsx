b4e9b114f4a1a9ef64fe9ab362289cf2bbe1fcfd
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-13 23:11:31 -0700

    chore: add hobbies

--- app/routes/about/components/index/index.tsx
+++ app/routes/about/components/index/index.tsx
@@ -7,12 +7,10 @@ import {
   ListItem,
   Text,
 } from "@chakra-ui/react";
-import { FaLaptop, FaBook } from "react-icons/fa";
-import { IoFastFoodOutline } from "react-icons/io5";
 import { useIndex } from "./useIndex";
 
 export const About = () => {
-  const { t } = useIndex();
+  const { t, hobbies } = useIndex();
 
   return (
     <Box py="20px" px="24px">
@@ -26,44 +24,21 @@ export const About = () => {
         {t("I like to do")}
       </Heading>
       <List spacing="24px">
-        <ListItem>
-          <Flex alignItems="center" gap="16px" mb="8px">
-            <ListIcon boxSize="32px" as={FaLaptop} color="green.500" m={0} />
-            <Heading as="h4" mb="16px" m={0} fontWeight={500}>
-              {t("software development.title")}
-            </Heading>
-          </Flex>
-          <Text>{t("software development.description")}</Text>
-        </ListItem>
-        <ListItem>
-          <Flex alignItems="center" gap="16px" mb="8px">
-            <ListIcon boxSize="32px" as={FaBook} color="green.500" m={0} />
-            <Heading as="h4" mb="16px" m={0} fontWeight={500}>
-              {t("reading comics and watching anime.title")}
-            </Heading>
-          </Flex>
-          <Text>
-            {t("reading comics and watching anime.description1")}
-            <br />
-            {t("reading comics and watching anime.description2")}
-            <br />
-            {t("reading comics and watching anime.description3")}
-          </Text>
-        </ListItem>
-        <ListItem>
-          <Flex alignItems="center" gap="16px" mb="8px">
-            <ListIcon
-              boxSize="32px"
-              as={IoFastFoodOutline}
-              color="green.500"
-              m={0}
-            />
-            <Heading as="h4" mb="16px" m={0} fontWeight={500}>
-              {t("going to a restaurant.title")}
-            </Heading>
-          </Flex>
-          <Text>{t("going to a restaurant.description")}</Text>
-        </ListItem>
+        {hobbies.map(({ icon, title, descriptions }) => {
+          return (
+            <ListItem key={title}>
+              <Flex alignItems="center" gap="16px" mb="8px">
+                <ListIcon boxSize="32px" as={icon} color="green.500" m={0} />
+                <Heading as="h4" mb="16px" m={0} fontWeight={500}>
+                  {title}
+                </Heading>
+              </Flex>
+              {descriptions.map((description) => {
+                return <Text key={description}>{description}</Text>;
+              })}
+            </ListItem>
+          );
+        })}
       </List>
     </Box>
   );
--- app/routes/about/components/index/useIndex.ts
+++ app/routes/about/components/index/useIndex.ts
@@ -1,7 +1,31 @@
 import { useTranslation } from "react-i18next";
+import { FaBook, FaLaptop } from "react-icons/fa";
+import { IoFastFoodOutline } from "react-icons/io5";
 
 export const useIndex = () => {
   const { t } = useTranslation("about");
 
-  return { t };
+  const hobbies = [
+    {
+      icon: FaLaptop,
+      title: t("software development.title"),
+      descriptions: [t("software development.description")],
+    },
+    {
+      icon: FaBook,
+      title: t("reading comics and watching anime.title"),
+      descriptions: [
+        t("reading comics and watching anime.description1"),
+        t("reading comics and watching anime.description2"),
+        t("reading comics and watching anime.description3"),
+      ],
+    },
+    {
+      icon: IoFastFoodOutline,
+      title: t("going to a restaurant.title"),
+      descriptions: [t("going to a restaurant.description")],
+    },
+  ];
+
+  return { t, hobbies };
 };
