f0d6ae7f6a18c8ab5b761678dace7c90974ed48c
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-15 15:52:40 -0700

    feat: add meta to login page

--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -1,5 +1,12 @@
-import { type ActionFunctionArgs } from "@remix-run/node";
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import i18next from "~/i18n/i18next.server";
 import { authenticator } from "~/utils/auth.server";
+import { createMetaTitle } from "~/utils/createMetaTitle";
 import { Login } from "./components/index";
 
 export default function Route() {
@@ -9,5 +16,15 @@ export default function Route() {
 export function action({ request }: ActionFunctionArgs) {
   return authenticator.authenticate("auth0", request);
 }
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const t = await i18next.getFixedT(request, "login");
+  const title = t("Login");
+  return json({ title });
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: createMetaTitle(data?.title ?? "") }];
+};
 
 export const handle = { i18n: "login" };
