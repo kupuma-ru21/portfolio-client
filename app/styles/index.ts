bf1722a2cc43df23ac3456050e6ed81aa1d4190d
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-16 14:44:01 -0700

    fix: show scroll bar all the time

--- app/styles/index.ts
+++ app/styles/index.ts
@@ -1,6 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const styles = { global: { "html, body": { overscrollBehavior: "none" } } };
+const styles = {
+  global: {
+    "html, body": { overscrollBehavior: "none" },
+    // NOTE UI changes if scroll bar is present so we force it to always be present
+    html: { overflowY: "scroll" },
+  },
+};
 
 const config = { initialColorMode: "light", useSystemColorMode: false };
 
