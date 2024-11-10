30fd4c6090e027a3564e69ad27e2cf27d272fa8f
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-11-10 14:42:41 -0800

    chore: wip

--- client/app/routes/_index/route.tsx
+++ client/app/routes/_index/route.tsx
@@ -12,6 +12,7 @@ import { get500ErrorResponse } from "~/utils/error/get500ErrorResponse";
 import { apolloClient } from "~/utils/graphql";
 
 export default function Route() {
+  console.log("test");
   // const data = useLoaderData<typeof loader>();
   return (
     <Index
